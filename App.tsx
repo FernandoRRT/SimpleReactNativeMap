46c7a1a2910aced2278992e902f7cef4261e5549
Fernando <suporteiros@gmail.com>
2022-07-28 22:39:13 -0300

    Details screen
    The POi details map

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/Home/Home';
+import Detail from './src/Detail/Detail';
+
+export type RootStackParamList = {
+  Home: undefined;
+  Detail: object;
+}
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Detail" component={Detail} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/Home/dbFetch.tsx
@@ -0,0 +1,30 @@
+export const dbFetch = [
+    {
+        category: "gas",
+        contact: "11 9999999",
+        description: "Nunca fique parado na pista",
+        id: "78787d878sadsa8sds8dad",
+        latitude: -23.54729127842035,
+        longitude: -46.65369848714639,
+        name: "Combustível na mão"
+    },
+    {
+        category: "restaurant",
+        contact: "11 989898955484",
+        description: "Entregando pães com carinho",
+        id: "635b8528-d08a-4998-8c85-fd7a2109051c",
+        latitude: -23.54957184424356,
+        longitude: -46.61749944090844,
+        name: "Padaria do João"
+    },
+    {
+        category: "pet",
+        contact: "11 9999999",
+        description: "Seu gato sempre limpinho",
+        id: "2509c576-b965-4c56-b1ba-4132e2b4d0a3",
+        latitude: 23.542362394785165,
+        longitude: -46.61749944090844,
+        name: "Petshop cat"
+    },
+
+];
